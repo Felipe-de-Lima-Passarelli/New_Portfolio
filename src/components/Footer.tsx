6ace8710756ef109891960879d1f6804f8f5f5b5
Felipe de Lima Passarelli <malamdorox@gmail.com>
2026-07-02 04:55:24 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,16 @@
 const Footer = () => {
   return (
-    <footer className="text-white text-center py-6 bg-[#477AF5] rounded-t-[40px] w-full">
-      <p>
-        © {new Date().getFullYear()} • Projeto desenvolvido por
-        <a href="https://github.com/Felipe-de-Lima-Passarelli" target="_blank">
-          <span className="font-semibold ml-1">Felipe de Lima Passarelli</span>
+    <footer className="text-white text-center py-6 px-4 bg-[#477AF5] rounded-t-[40px] w-full">
+      <p className="text-sm sm:text-base">
+        © {new Date().getFullYear()} • Portfólio desenvolvido por
+        <a
+          href="https://github.com/Felipe-de-Lima-Passarelli"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Acessar GitHub de Felipe de Lima Passarelli"
+          className="font-semibold hover:underline"
+        >
+          Felipe de Lima Passarelli
         </a>
       </p>
     </footer>
